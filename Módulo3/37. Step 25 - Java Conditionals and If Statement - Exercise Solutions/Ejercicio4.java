0d9353b5e9dac86bcdd1e1fff227fc0daedfb4c2
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-14 22:44:26 -0500

    Modulo3 completado

---
+++ Módulo3/37. Step 25 - Java Conditionals and If Statement - Exercise Solutions/Ejercicio4.java
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------//
+//1.Have four variables a, b, c, d and create a statement for print if a + b is greater than c + d
+int a = 5;
+int b = 2;
+int c = 1;
+int d = 3;
+
+int primersuma = a+b
+int segundsuma = c+d
+
+if(primersuma > segundsuma ){
+    System.out.printf("a+b = %d is greater than c+d = %d", primersuma, segundsuma).println();
+}
+
+//-------------------------------------------------------------//
+//2.Have three angles of a triangle stored in three variables angle1, angle2, angle3.
+//Create a if statement to state if the three angles can form a triangle. Hint: angle1 + angle2 + angle3 = 180
+int angle1 = 20;
+int angle2 = 60;
+int angle3 = 100;
+
+if( angle1 + angle2 + angle3 ==180){
+    System.out.println("Valid Triangle");
+}
+
+//-------------------------------------------------------------//
+//3.Have a variable called number. Create a if statement to find if it is an even number. Hint: % operator
+int number = 10;
+
+if(number % 2 == 0){
+    System.out.println("Number is even");
+}
