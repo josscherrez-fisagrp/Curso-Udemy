20b323967ca5fa4aeded5cefd65b587f5b0c74bb
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-14 21:27:22 -0500

    Modulo3 completado

---
+++ Módulo3/14. Step 05 - Java Expression - Exercises/Ejercicio1.java
@@ -0,0 +1,6 @@
+//-------------------------------------------------------------//
+//1. Write an expression to calculate number of minutes in a day
+60*24
+//-------------------------------------------------------------//
+//2. Write an expression to calculate number of seconds in a day
+60*60*24
