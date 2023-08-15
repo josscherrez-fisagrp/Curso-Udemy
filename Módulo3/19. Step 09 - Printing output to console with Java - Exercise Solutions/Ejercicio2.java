364234496517075ecc8a89d9111d5d1870bad1a8
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-14 21:45:23 -0500

    Modulo3 completado

---
+++ Módulo3/19. Step 09 - Printing output to console with Java - Exercise Solutions/Ejercicio2.java
@@ -0,0 +1,18 @@
+//-------------------------------------------------------------//
+//1.Print hello world
+System.out.println("Hello World")
+//-------------------------------------------------------------//
+//2.Print 5 * 3 as is
+System.out.println("5 * 3")
+//-------------------------------------------------------------//
+//3.Print calculated value of 5 * 3
+System.out.println(5*3)
+//-------------------------------------------------------------//
+//4.How to print mathematical table of 5 now?
+//En este caso se podría utilizar un for para imprimir la tabla de 5
+for(int i = 1;i<=10;i++){
+    System.out.println("5* " +i+ "=" + 5*i);
+}
+//-------------------------------------------------------------//
+//5.Print number of seconds in a day using System.out.println
+System.out.println(60 * 60 * 24)
