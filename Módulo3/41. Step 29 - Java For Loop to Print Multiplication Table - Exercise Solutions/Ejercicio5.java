2d5c8dd71182a08eddc38596b40c68bd58bf4d18
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-15 10:58:55 -0500

    Modulo3 completado

--- Módulo3/41. Step 29 - Java For Loop to Print Multiplication Table - Exercise Solutions/Ejercicio5.java
+++ Módulo3/41. Step 29 - Java For Loop to Print Multiplication Table - Exercise Solutions/Ejercicio5.java
@@ -1,58 +1,51 @@
 //-------------------------------------------------------------//
 //1.Do the whole thing again with 7 table. Kill Jshell. Repeat. Rinse. Start Again.
-
-
-
-
-
-
+for (int i = 1; i <= 10; i++) {
+    System.out.printf("%d * %d = %d", 7, i, 7 * i).println();
+}
 
 //-------------------------------------------------------------//
 //2.Print 6 and 10 tables!
+int table = 6;
 
+for (int i = 1; i <= 10; i++) {
+    System.out.printf("%d * %d = %d", table, i, table * i).println();
+}
 
+int table2= 10;
 
-
-
+for (int i = 1; i <= 10; i++) {
+    System.out.printf("%d * %d = %d", table2, i, table2 * i).println();
+}
 
 //-------------------------------------------------------------//
 //3.Print numbers from 1 to 10
-
-
-
-
-
+for (int i = 1; i <= 10; i++) {
+  System.out.printf("%d", i).println();
+}
 
 //-------------------------------------------------------------//
 //4.Print numbers from 10 to 1
-
-
-
-
-
-
+for (int i = 10; i >= 1; i--) {
+  System.out.printf("%d", i).println();
+}
 
 //-------------------------------------------------------------//
 //5.Print squares of First 10 Numbers
-
-
-
-
-
-
+for(int i = 1; i <= 10; i++) {
+  System.out.printf("%d", i * i).println();
+}
 
 //-------------------------------------------------------------//
 //6.Print squares of First 10 Even Numbers
-
-
-
-
-
-
-
+for(int i = 2; i <= 20; i=i+2) {
+  System.out.printf("%d", i * i).println();
+}
 
 //-------------------------------------------------------------//
 //7.Print squares of First 10 Odd Numbers
-
+for(int i = 1; i <= 20; i=i+2) {
+  System.out.printf("%d", i * i).println();
+}
 
 
