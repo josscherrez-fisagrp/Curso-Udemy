7d543f02f84596a35fc7da0eb25183aeae84f21b
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-14 21:58:50 -0500

    Modulo3 completado

---
+++ Módulo3/25. Step 14 - Introduction to Variables in Java - Exercises and Puzzles/Ejercicio3.java
@@ -0,0 +1,14 @@
+//-------------------------------------------------------------//
+//1.Create three integer variables a, b & c. Create a statement for printing sum of three variables. 
+//Modify the value of a. Print value of a + b + c. Modify the value of b. Print value of a + b + c
+int a = 10;
+int b = 20;
+int c = 30;
+
+System.out.printf("%d + %d + %d = %d",a,b,c,a+b+c).println()
+//Change int a = 50
+int a = 50;
+System.out.printf("%d + %d + %d = %d",a,b,c,a+b+c).println()
+//Change int b = 70
+int b = 70;
+System.out.printf("%d + %d + %d = %d",a,b,c,a+b+c).println()
