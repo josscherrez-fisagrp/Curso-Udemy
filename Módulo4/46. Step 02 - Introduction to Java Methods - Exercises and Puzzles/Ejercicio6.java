1dbbcc2a22881a866d41059318ee0585f36e0703
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-15 11:29:03 -0500

    Modulo4 completado

---
+++ Módulo4/46. Step 02 - Introduction to Java Methods - Exercises and Puzzles/Ejercicio6.java
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------//
+//1.Create sayHelloWorldThrice() method and execute it
+
+void sayHelloWorldThrice() {
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+}
+//Ejecutar método
+sayHelloWorldThrice();
+
+//-------------------------------------------------------------//
+//2.Create a method which prints four statements and execute it
+
+void printLearningExperience(){
+    System.out.println("I've created my first variable");
+    System.out.println("I've created my first method");
+    System.out.println("I've created my first loop");
+    System.out.println("I'm excited to learn Java");
+}
+
+printLearningExperience();
