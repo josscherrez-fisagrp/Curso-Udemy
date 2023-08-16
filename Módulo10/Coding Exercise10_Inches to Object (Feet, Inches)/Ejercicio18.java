9e8020d286d165c55d92f8ca42fb7348ab3a36e4
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-15 19:02:45 -0500

    Modulo10 completado

---
+++ Módulo10/Coding Exercise10_Inches to Object (Feet, Inches)/Ejercicio18.java
@@ -0,0 +1,26 @@
+public class Dimension {
+    private int feet;
+    private int inches;
+    
+    public Dimension(int inches) {
+        if(inches <0 ){
+            this.feet = -1;
+            this.inches = -1;
+        }else{
+            this.feet = inches/12;
+            this.inches = inches %12;
+        }
+        // TODO: Convert the total inches into feet and inches. One foot is 12 inches.
+        // Store the feet and inches in their respective instance variables.
+    }
+    
+    public int getFeet() {
+        // TODO: Return the value of feet.
+        return feet;
+    }
+    
+    public int getInches() {
+        // TODO: Return the value of inches.
+        return inches;
+    }    
+}
---
+++ Módulo10/Coding Exercise11_Create a Square class/Ejercicio19.java
@@ -0,0 +1,28 @@
+public class Square {
+
+    private int side;
+
+    public Square(int side) {
+        // TODO: Initialize side with the passed value
+        this.side = side;
+        
+    }
+
+    public int calculateArea() {
+        // TODO: Calculate and return the area of the square
+        if(side <=0){
+            return -1;
+        }
+        return side *side;
+    }
+
+    public int calculatePerimeter() {
+        // TODO: Calculate and return the perimeter of the square
+        if(side <=0){
+            return -1;
+        }
+        return 4 *side;
+      
+    }
+
+}
