51aa0c5d53677d5ef3e52b107b84c746643af1cb
josscherrez-fisagrp <joselyn.cherrez@fisagrp.com>
2023-08-16 20:26:11 -0500

    Modulo32 completado

---
+++ Módulo32/Coding Exercise46_Enum Day Of The Week/Ejercicio54.java
@@ -0,0 +1,38 @@
+enum DaysOfWeek {
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday");
+
+    private String name;
+
+    DaysOfWeek(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        // Write your code here to return the name of the day
+        return this.name;
+    }
+
+    public boolean isWeekday() {
+        // Write your code here to check if this day is a weekday
+        return !isHoliday();
+    }
+
+    public boolean isHoliday() {
+        // Write your code here to check if this day is a holiday
+        switch (this) {
+            
+            case SATURDAY, SUNDAY:
+                return true;
+            default:
+                return false;
+                
+            
+        }
+    }
+}
